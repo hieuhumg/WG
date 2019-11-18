68248d30355b5bad14a2cf6f63bac6d73b6409bf
hieuhumg <hieu.nikon2711@gmail.com>
2019-11-18 18:15:27 +0700

    abc

---
+++ adamo/js/setup.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+   //owl caraousel
+    $('#slider-index').owlCarousel({
+        items:1,
+        loop:true,
+        margin:0,
+        dots: true,
+        navigation: true,
+        autoplay:false,
+        autoplayTimeout:4000,
+        // autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                autoplay:false,
+            },
+            1200: {
+                dots: true
+            }
+        }
+    });
+});
